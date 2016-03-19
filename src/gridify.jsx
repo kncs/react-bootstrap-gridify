8245fc15789f29087d7cd50a797e8ee46648b555
kncs <kevin.clarens@gmail.com>
2016-03-19 22:32:23 +0100

    fix(gridify.jsx): add offset in order to center result => depending on columns number

--- src/gridify.jsx
+++ src/gridify.jsx
@@ -31,38 +31,45 @@ function Row(props) {
 
 export default class Gridify extends React.Component {
 
-  calculBoostrapColSize() {
+  calculBoostrapColProperties() {
     let size = 1;
+    let offset = 0;
     if(acceptedColNumber.indexOf(this.props.columns) !== -1) {
-      size = maxBootstrapCols/this.props.columns;
+      size = Math.floor(maxBootstrapCols/this.props.columns);
+      offset = Math.floor((maxBootstrapCols%this.props.columns)/2);
     } else {
-      if(this.props.columns > maxBootstrapCols) {
+      if(this.props.columns >= maxBootstrapCols) {
         size = 1;
       } else {
         size = 12;
       }
     }
-    return size;
+    return {
+      size :size,
+      offset: offset
+    };
   }
 
   render() {
     let self = this;
-    let bootstrapColSize = this.calculBoostrapColSize();
+    let colProperties = this.calculBoostrapColProperties();
     let colSize = this.props.colSize ? this.props.colSize : 'sm';
     let complementClass = this.props.className ? ' ' + this.props.className : '';
-    let className = 'col-'+ colSize +'-' + bootstrapColSize + complementClass;
+    let className ='col-'+colSize +'-'+colProperties.size+complementClass;
+    let classNameWithOffset ='col-'+colSize +'-offset-'+colProperties.offset+' '+className;
     return (
       <div>
       {
         this.props.components
         .reduce(function(prec, current, index){
-          let component = (
-            <div className={className} key={'Col'+index} >{current}</div>
-          );
           if(index%self.props.columns === 0) {
-            prec.push([component]);
+            prec.push([
+              <div className={classNameWithOffset} key={'Col'+index} >{current}</div>
+            ]);
           } else {
-            prec[Math.floor(index/self.props.columns)].push(component);
+            prec[Math.floor(index/self.props.columns)].push(
+              <div className={className} key={'Col'+index} >{current}</div>
+            );
           }
           return prec;
         }, [])
